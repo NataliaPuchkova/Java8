ebd19719edf37f7a9d193d3d5c1161d17c2d6e26
Natalia Puchkova <35714707+NataliaPuchkova@users.noreply.github.com>
2019-01-17 10:53:27 -0600

    Update Example.java

--- Example.java
+++ Example.java
@@ -5,10 +5,21 @@ class Example {
 
  // Create set of object's fields from the list of objects
   public HashSet<String> getFormat(String time, ArrayList<Subscriptor> subs){
-    return (HashSet<String> subs.strewam()
+    return (HashSet<String> subs.stream()
                                  .filter(x->x.getTime().equals(time))
                                  .map(y->y.getFormat())
                                  .collect(Collectors.toSet());
   
   } 
+  //Create set from subsets in stream          
+  public HashSet<String>getTime (String var, ArrayList<Account> accountList){
+   return (HashSet<String>) accountList.stream()
+                                       .filter(x->x.getSomething().equals(var))
+                                       .map(y->y.getSubscriptors()
+                                                .stream()
+                                                .map(z->z.getFormat())
+                                                .collect(Collectors.toSet()))
+                                       .flatMap(Set::stream)
+                                       .collect(Collectors.toSet());
+  }         
 }
